bcf5d0f527308f88d1018a5312d471ea26cda8b1
Rafael Borges <rafaelfborges@gmail.com>
2020-03-01 01:14:25 -0300

    feat: Add course rating star component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { StarComponent } from "./star/star.component";
 import { CourseListComponent } from "./courses/course-list.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
@@ -6,7 +7,7 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent],
+  declarations: [AppComponent, CourseListComponent, StarComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
