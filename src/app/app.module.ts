7af075e7e90a3296fc38b1f8de700ec1fe4757c1
Rafael Borges <rafaelfborges@gmail.com>
2020-03-03 14:13:38 -0300

    feat: Add navbar and 404 component, setting up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,43 @@
-import { ReplacePipe } from "./pipe/replace.pipe";
-import { StarComponent } from "./star/star.component";
-import { CourseListComponent } from "./courses/course-list.component";
+import { Error404Component } from "./error-404/error-404.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
+import { CourseListComponent } from "./courses/course-list.component";
+import { StarComponent } from "./star/star.component";
+import { ReplacePipe } from "./pipe/replace.pipe";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @NgModule({
-  declarations: [AppComponent, CourseListComponent, StarComponent, ReplacePipe],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        redirectTo: "courses",
+        pathMatch: "full"
+      },
+      {
+        path: "courses",
+        component: CourseListComponent
+      },
+      {
+        path: "**",
+        component: Error404Component
+      }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
